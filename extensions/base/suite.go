a2d319d2e6f6307b02f95cff2a7e536b3d5e6a82
NSMBot <nsmbot@networkservicmesh.io>
2021-04-01 05:27:48 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main networkservicemesh/deployments-k8s#
    networkservicemesh/deployments-k8s PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    
    networkservicemesh/deployments-k8s commit message:
    commit 1f4866506f574e833e6bd4eacc4fc5247fa25123
    Author: Network Service Mesh Bot <60070799+nsmbot@users.noreply.github.com>
    Date:   Thu Apr 1 00:24:03 2021 -0500
    
        Update from update/networkservicemesh/deployments-k8s
    
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- extensions/base/suite.go
+++ extensions/base/suite.go
@@ -30,7 +30,7 @@ type Suite struct {
 
 func (s *Suite) SetupSuite() {
 	s.Repository = "networkservicemesh/deployments-k8s"
-	s.Version = "e32ded3c"
+	s.Version = "eee66a53"
 	s.Dir = "../" // Note: this should be synced with input parameters in gen.go file
 
 	s.Suite.SetupSuite()
