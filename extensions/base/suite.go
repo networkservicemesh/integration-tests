9b419e5aa3dc09d89495530930866ee495dccc71
NSMBot <nsmbot@networkservicmesh.io>
2021-06-22 18:34:12 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main networkservicemesh/deployments-k8s#
    networkservicemesh/deployments-k8s PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    
    networkservicemesh/deployments-k8s commit message:
    commit 5dd95c3d9fc4a90624e0b3244d96618cacaae54b
    Author: Network Service Mesh Bot <60070799+nsmbot@users.noreply.github.com>
    Date:   Tue Jun 22 13:30:25 2021 -0500
    
        Update go.mod and go.sum to latest version from networkservicemesh/de…
    
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- extensions/base/suite.go
+++ extensions/base/suite.go
@@ -51,7 +51,7 @@ func (s *Suite) TearDownSuite() {
 // SetupSuite runs all extensions
 func (s *Suite) SetupSuite() {
 	s.checkout.Repository = "networkservicemesh/deployments-k8s"
-	s.checkout.Version = "9c681772"
+	s.checkout.Version = "9c7741ac"
 	s.checkout.Dir = "../" // Note: this should be synced with input parameters in gen.go file
 
 	s.checkout.SetT(s.T())
