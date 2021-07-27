d4081124c48679617fa32d41d369d0ac1ed9c5d3
NSMBot <nsmbot@networkservicmesh.io>
2021-07-27 10:11:23 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main networkservicemesh/deployments-k8s#
    networkservicemesh/deployments-k8s PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    
    networkservicemesh/deployments-k8s commit message:
    commit 14bc9d08a7f46bea7273dcf8d8fd5d6a5837b1ef
    Author: Ed Warnicke <hagbard@gmail.com>
    Date:   Mon Jul 26 11:48:07 2021 -0500
    
        Merge pull request #695 from glazychev-art/bug/issue-694
    
        Save logs from all pod containers
    
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- extensions/base/suite.go
+++ extensions/base/suite.go
@@ -51,7 +51,7 @@ func (s *Suite) TearDownSuite() {
 // SetupSuite runs all extensions
 func (s *Suite) SetupSuite() {
 	s.checkout.Repository = "networkservicemesh/deployments-k8s"
-	s.checkout.Version = "e2b8087d"
+	s.checkout.Version = "99784703"
 	s.checkout.Dir = "../" // Note: this should be synced with input parameters in gen.go file
 
 	s.checkout.SetT(s.T())
