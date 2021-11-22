b87f1382e4c17e27d37a3e789ffac3321b3b25c0
Mikhail Avramenko <avramenkomihail15@gmail.com>
2021-11-22 18:45:35 +0700

    test annotations
    Signed-off-by: Mikhail Avramenko <avramenkomihail15@gmail.com>

--- suites/spire/suite.gen.go
+++ suites/spire/suite.gen.go
@@ -25,7 +25,7 @@ func (s *Suite) SetupSuite() {
 	s.T().Cleanup(func() {
 		r.Run(`kubectl delete crd spiffeids.spiffeid.spiffe.io` + "\n" + `kubectl delete ns spire`)
 	})
-	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/spire?ref=b67771cd2deebad0246c508ae7e501778d1e8443`)
+	r.Run(`kubectl apply -k ../../../../../../../../../Users/user/Documents/dev/nsm/deployments-k8s/examples/spire`)
 	r.Run(`kubectl wait -n spire --timeout=1m --for=condition=ready pod -l app=spire-agent`)
 	r.Run(`kubectl wait -n spire --timeout=1m --for=condition=ready pod -l app=spire-server`)
 }
