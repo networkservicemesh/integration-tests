0fd14b9e1b2a40dab6b674a5d42dedcaa01519f0
NSMBot <nsmbot@networkservicmesh.io>
2022-02-11 20:47:36 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main PR link: https://github.com/networkservicemesh/deployments-k8s/pull/4720
    Commit: 29bcf01
    Author: Network Service Mesh Bot
    Date: 2022-02-11 14:36:31 -0600
    Message:
      - Update go.mod and go.sum to latest version from networkservicemesh/de…
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- extensions/base/suite.go
+++ extensions/base/suite.go
@@ -52,7 +52,7 @@ func (s *Suite) TearDownSuite() {
 }
 
 const (
-	sha = "e7f539108cb5ff0f81a29879d079ad9f4f29fa3e"
+	sha = "f6c30463e01b0e38b1a867fafc96c4d29a201b76"
 )
 
 // SetupSuite runs all extensions
