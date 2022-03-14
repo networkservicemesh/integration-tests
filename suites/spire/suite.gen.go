69117769630b417fd65e1fe0359c3ac035d6c9fb
NSMBot <nsmbot@networkservicmesh.io>
2022-03-14 11:15:32 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main PR link: https://github.com/networkservicemesh/deployments-k8s/pull/4867
    Commit: ee7549b
    Author: Network Service Mesh Bot
    Date: 2022-03-13 22:32:45 -0500
    Message:
      - Update go.mod and go.sum to latest version from networkservicemesh/de…
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- suites/spire/suite.gen.go
+++ suites/spire/suite.gen.go
@@ -25,7 +25,7 @@ func (s *Suite) SetupSuite() {
 	s.T().Cleanup(func() {
 		r.Run(`kubectl delete crd spiffeids.spiffeid.spiffe.io` + "\n" + `kubectl delete ns spire`)
 	})
-	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/spire?ref=c2e44ad884573fea1eb971bc7a2044f56108af67`)
+	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/spire?ref=3a72707a60caafe70525ddcd2568e99fdac5a125`)
 	r.Run(`kubectl wait -n spire --timeout=1m --for=condition=ready pod -l app=spire-agent`)
 	r.Run(`kubectl wait -n spire --timeout=1m --for=condition=ready pod -l app=spire-server`)
 }
