720dd79ccd3f11b9b0df3bd957173fe7743151ae
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-03-24 18:34:09 +0700

    add observability and spire logs
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- extensions/logs/logs.go
+++ extensions/logs/logs.go
@@ -25,7 +25,6 @@ import (
 	"os/signal"
 	"path/filepath"
 	"regexp"
-	"strings"
 	"sync"
 	"syscall"
 	"time"
@@ -61,7 +60,7 @@ type Config struct {
 	ArtifactsDir      string        `default:"logs" desc:"Directory for storing container logs" envconfig:"ARTIFACTS_DIR"`
 	Timeout           time.Duration `default:"5s" desc:"Context timeout for kubernetes queries" split_words:"true"`
 	WorkerCount       int           `default:"8" desc:"Number of log collector workers" split_words:"true"`
-	AllowedNamespaces string        `default:"(ns-.*)|(nsm-system)" desc:"Regex of allowed namespaces" split_words:"true"`
+	AllowedNamespaces string        `default:"(ns-.*)|(nsm-system)|(spire)|(observability)" desc:"Regex of allowed namespaces" split_words:"true"`
 }
 
 func savePodLogs(ctx context.Context, pod *corev1.Pod, opts *corev1.PodLogOptions, fromInitContainers bool, dir string) {
@@ -232,7 +231,7 @@ func filterNamespaces(nsList *corev1.NamespaceList) []string {
 	var rv []string
 
 	for i := 0; i < len(nsList.Items); i++ {
-		if (nsList.Items[i].Name == "spire" || nsList.Items[i].Name == "nsm-system" || strings.HasPrefix(nsList.Items[i].Name, "ns-")) && nsList.Items[i].Status.Phase == corev1.NamespaceActive {
+		if matchRegex.MatchString(nsList.Items[i].Name) && nsList.Items[i].Status.Phase == corev1.NamespaceActive {
 			rv = append(rv, nsList.Items[i].Name)
 		}
 	}
