3a9c2a4688a7e9f3a388073d02bdb697d40a7c6c
NSMBot <nsmbot@networkservicmesh.io>
2022-05-14 20:23:02 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main PR link: https://github.com/networkservicemesh/deployments-k8s/pull/5999
    Commit: 472501f
    Author: Network Service Mesh Bot
    Date: 2022-05-14 15:18:40 -0500
    Message:
      - Update go.mod and go.sum to latest version from networkservicemesh/de…
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- extensions/base/suite.go
+++ extensions/base/suite.go
@@ -52,7 +52,7 @@ func (s *Suite) TearDownSuite() {
 }
 
 const (
-	sha = "2268bd62c0acfea5213a6b270565fa18ec06a1ff"
+	sha = "d73a90f6e21b00ede40e839ec4742b6fc718b377"
 )
 
 // SetupSuite runs all extensions
--- suites/spire/suite.gen.go
+++ suites/spire/suite.gen.go
@@ -25,7 +25,7 @@ func (s *Suite) SetupSuite() {
 	s.T().Cleanup(func() {
 		r.Run(`kubectl delete crd spiffeids.spiffeid.spiffe.io` + "\n" + `kubectl delete ns spire`)
 	})
-	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/spire?ref=cedd1a0e7befa3e188870cfa3e6200a7199178be`)
+	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/spire?ref=f1ac38126fccd6b12dc80d2426094d8fb63ec129`)
 	r.Run(`kubectl wait -n spire --timeout=2m --for=condition=ready pod -l app=spire-agent`)
 	r.Run(`kubectl wait -n spire --timeout=1m --for=condition=ready pod -l app=spire-server`)
 }
