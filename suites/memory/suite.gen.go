fd24b2d3fb397d944fbcce4956bb47d4feec0e67
Nikita Skrynnik <nikita.skrynnik@xored.com>
2023-07-31 20:21:04 +0700

    cleanup
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- suites/memory/suite.gen.go
+++ suites/memory/suite.gen.go
@@ -41,3 +41,25 @@ func (s *Suite) TestKernel2ethernet2kernel() {
 	r.Run(`kubectl exec pods/alpine -n ns-kernel2ethernet2kernel -- ping -c 4 172.16.1.100`)
 	r.Run(`kubectl exec deployments/nse-kernel -n ns-kernel2ethernet2kernel -- ping -c 4 172.16.1.101`)
 }
+func (s *Suite) TestKernel2kernel() {
+	r := s.Runner("../deployments-k8s/examples/memory/Kernel2Kernel")
+	s.T().Cleanup(func() {
+		r.Run(`kubectl delete ns ns-kernel2kernel`)
+	})
+	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/memory/Kernel2Kernel?ref=9f024f0af7d1988c25c0aeccd121f24af3c97c42`)
+	r.Run(`kubectl wait --for=condition=ready --timeout=1m pod -l app=alpine -n ns-kernel2kernel`)
+	r.Run(`kubectl wait --for=condition=ready --timeout=1m pod -l app=nse-kernel -n ns-kernel2kernel`)
+	r.Run(`kubectl exec pods/alpine -n ns-kernel2kernel -- ping -c 4 172.16.1.100`)
+	r.Run(`kubectl exec deployments/nse-kernel -n ns-kernel2kernel -- ping -c 4 172.16.1.101`)
+}
+func (s *Suite) TestMemif2memif() {
+	r := s.Runner("../deployments-k8s/examples/memory/Memif2Memif")
+	s.T().Cleanup(func() {
+		r.Run(`kubectl delete ns ns-memif2memif`)
+	})
+	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/memory/Memif2Memif?ref=9f024f0af7d1988c25c0aeccd121f24af3c97c42`)
+	r.Run(`kubectl wait --for=condition=ready --timeout=1m pod -l app=nsc-memif -n ns-memif2memif`)
+	r.Run(`kubectl wait --for=condition=ready --timeout=1m pod -l app=nse-memif -n ns-memif2memif`)
+	r.Run(`result=$(kubectl exec deployments/nsc-memif -n ns-memif2memif -- vppctl ping 172.16.1.100 repeat 4)` + "\n" + `echo ${result}` + "\n" + `! echo ${result} | grep -E -q "(100% packet loss)|(0 sent)|(no egress interface)"`)
+	r.Run(`result=$(kubectl exec deployments/nse-memif -n ns-memif2memif -- vppctl ping 172.16.1.101 repeat 4)` + "\n" + `echo ${result}` + "\n" + `! echo ${result} | grep -E -q "(100% packet loss)|(0 sent)|(no egress interface)"`)
+}
