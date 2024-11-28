30282df30fa940c7a2c4fac6946d1e0b365d1912
NSMBot <nsmbot@networkservicmesh.io>
2024-11-28 10:48:58 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12569
    Commit: 55c2163
    Author: Network Service Mesh Bot
    Date: 2024-11-28 04:21:44 -0600
    Message:
      - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#3982)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: f22c4f5
    Author: Network Service Mesh Bot
    Date: 2024-11-28 04:05:47 -0600
    Message:
        - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#3979)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12488
    Commit: a35d803
    Author: Denis Tingaikin
    Date: 2024-11-22 08:44:03 -0500
    Message:
            - Merge pull request #3962 from Ex4amp1e/update-go-1.23
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- suites/interdomain/two_cluster_configuration/basic/suite.gen.go
+++ suites/interdomain/two_cluster_configuration/basic/suite.gen.go
@@ -36,8 +36,8 @@ func (s *Suite) SetupSuite() {
 		r.Run(`kubectl --kubeconfig=$KUBECONFIG1 delete mutatingwebhookconfiguration nsm-mutating-webhook` + "\n" + `kubectl --kubeconfig=$KUBECONFIG1 delete ns nsm-system`)
 		r.Run(`kubectl --kubeconfig=$KUBECONFIG2 delete mutatingwebhookconfiguration nsm-mutating-webhook` + "\n" + `kubectl --kubeconfig=$KUBECONFIG2 delete ns nsm-system`)
 	})
-	r.Run(`kubectl --kubeconfig=$KUBECONFIG1 apply -k https://github.com/networkservicemesh/deployments-k8s/examples/interdomain/two_cluster_configuration/basic/cluster1?ref=b2f1cc94c129264f6dd1f529087e6ee1a6714564`)
-	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 apply -k https://github.com/networkservicemesh/deployments-k8s/examples/interdomain/two_cluster_configuration/basic/cluster2?ref=b2f1cc94c129264f6dd1f529087e6ee1a6714564`)
+	r.Run(`kubectl --kubeconfig=$KUBECONFIG1 apply -k https://github.com/networkservicemesh/deployments-k8s/examples/interdomain/two_cluster_configuration/basic/cluster1?ref=36af411c7cccdda22cbd6ca1e8ffe0c299ec4638`)
+	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 apply -k https://github.com/networkservicemesh/deployments-k8s/examples/interdomain/two_cluster_configuration/basic/cluster2?ref=36af411c7cccdda22cbd6ca1e8ffe0c299ec4638`)
 	r.Run(`kubectl --kubeconfig=$KUBECONFIG1 wait --for=condition=ready --timeout=1m pod -n nsm-system -l app=admission-webhook-k8s`)
 	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 wait --for=condition=ready --timeout=1m pod -n nsm-system -l app=admission-webhook-k8s`)
 }
--- suites/observability/dashboard/suite.gen.go
+++ suites/observability/dashboard/suite.gen.go
@@ -27,7 +27,7 @@ func (s *Suite) SetupSuite() {
 	s.T().Cleanup(func() {
 		r.Run(`pkill -f "kubectl port-forward -n nsm-system service/dashboard-backend 3001:3001"` + "\n" + `pkill -f "kubectl port-forward -n nsm-system service/dashboard-ui 3000:3000"` + "\n" + `kubectl delete service/dashboard-ui service/dashboard-backend pod/dashboard -n=nsm-system`)
 	})
-	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/apps/dashboard?ref=b2f1cc94c129264f6dd1f529087e6ee1a6714564`)
+	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/apps/dashboard?ref=36af411c7cccdda22cbd6ca1e8ffe0c299ec4638`)
 	r.Run(`kubectl wait --for=condition=ready pod -l app=dashboard --timeout=5m -n nsm-system`)
 	r.Run(`nohup kubectl port-forward -n nsm-system service/dashboard-backend 3001:3001 &`)
 	r.Run(`nohup kubectl port-forward -n nsm-system service/dashboard-ui 3000:3000 &`)
