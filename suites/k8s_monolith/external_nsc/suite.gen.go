55787733911081d6d215c5fa5dd7c55575ee5754
NSMBot <nsmbot@networkservicmesh.io>
2024-12-04 09:52:57 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12586
    Commit: c30c52e
    Author: Network Service Mesh Bot
    Date: 2024-12-04 03:52:41 -0600
    Message:
      - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#3988)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 8c9d6af
    Author: Network Service Mesh Bot
    Date: 2024-12-04 03:48:43 -0600
    Message:
        - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#3987)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12580
    Commit: 4c2ff5f
    Author: Network Service Mesh Bot
    Date: 2024-12-04 03:46:11 -0600
    Message:
            - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#3986)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: fe3035b
    Author: Network Service Mesh Bot
    Date: 2024-12-02 03:57:37 -0600
    Message:
                    - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#3985)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12570
    Commit: 6300f6e
    Author: Network Service Mesh Bot
    Date: 2024-11-29 04:19:54 -0600
    Message:
                                    - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#3984)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12571
    Commit: c668c88
    Author: Network Service Mesh Bot
    Date: 2024-11-28 04:50:17 -0600
    Message:
                                                      - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#3983)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12569
    Commit: 55c2163
    Author: Network Service Mesh Bot
    Date: 2024-11-28 04:21:44 -0600
    Message:
                                                                        - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#3982)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: f22c4f5
    Author: Network Service Mesh Bot
    Date: 2024-11-28 04:05:47 -0600
    Message:
                                                                                          - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#3979)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12488
    Commit: a35d803
    Author: Denis Tingaikin
    Date: 2024-11-22 08:44:03 -0500
    Message:
                                                                                                            - Merge pull request #3962 from Ex4amp1e/update-go-1.23
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- suites/k8s_monolith/external_nsc/suite.gen.go
+++ suites/k8s_monolith/external_nsc/suite.gen.go
@@ -35,7 +35,7 @@ func (s *Suite) SetupSuite() {
 	s.T().Cleanup(func() {
 		r.Run(`kubectl delete mutatingwebhookconfiguration nsm-mutating-webhook` + "\n" + `kubectl delete ns nsm-system`)
 	})
-	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/k8s_monolith/configuration/cluster?ref=637f4cb892d31a6b7f2859ccb2e7fccded63cc6a`)
+	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/k8s_monolith/configuration/cluster?ref=9357c91aafd5f7a7a885e0d87aef3a322a9cb9ad`)
 	r.Run(`kubectl get services registry -n nsm-system -o go-template='{{index (index (index (index .status "loadBalancer") "ingress") 0) "ip"}}'`)
 }
 func (s *Suite) TestKernel2IP2Kernel() {
@@ -43,7 +43,7 @@ func (s *Suite) TestKernel2IP2Kernel() {
 	s.T().Cleanup(func() {
 		r.Run(`kubectl delete ns ns-kernel2ip2kernel-monolith-nsc`)
 	})
-	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/k8s_monolith/external_nsc/usecases/Kernel2IP2Kernel?ref=637f4cb892d31a6b7f2859ccb2e7fccded63cc6a`)
+	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/k8s_monolith/external_nsc/usecases/Kernel2IP2Kernel?ref=9357c91aafd5f7a7a885e0d87aef3a322a9cb9ad`)
 	r.Run(`kubectl wait --for=condition=ready --timeout=1m pod -l app=nse-kernel -n ns-kernel2ip2kernel-monolith-nsc`)
 	r.Run(`docker exec nsc-simple-docker ping -c4 172.16.1.100`)
 	r.Run(`kubectl exec deployments/nse-kernel -n ns-kernel2ip2kernel-monolith-nsc -- ping -c 4 172.16.1.101`)
