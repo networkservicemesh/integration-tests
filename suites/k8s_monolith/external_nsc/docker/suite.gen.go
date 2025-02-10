4669551d34b8767bb1388173ab96aad285ed44ac
NSMBot <nsmbot@networkservicmesh.io>
2025-02-10 09:44:48 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main PR link: https://github.com/networkservicemesh/deployments-k8s/pull/8884
    Commit: 99d1c15
    Author: Network Service Mesh Bot
    Date: 2025-02-10 03:43:50 -0600
    Message:
      - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4050)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12695
    Commit: 7889951
    Author: Network Service Mesh Bot
    Date: 2025-02-06 09:00:35 -0600
    Message:
        - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4049)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12697
    Commit: cb7a6f2
    Author: Network Service Mesh Bot
    Date: 2025-02-06 07:48:32 -0600
    Message:
            - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4048)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 54e06ce
    Author: Network Service Mesh Bot
    Date: 2025-02-04 11:43:09 -0600
    Message:
                    - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4047)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12692
    Commit: ecf04b2
    Author: Network Service Mesh Bot
    Date: 2025-01-31 00:23:29 -0600
    Message:
                                    - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4046)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12435
    Commit: 01fa915
    Author: Network Service Mesh Bot
    Date: 2025-01-30 04:35:43 -0600
    Message:
                                                      - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4045)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12691
    Commit: 7c6ab71
    Author: Network Service Mesh Bot
    Date: 2025-01-29 04:27:57 -0600
    Message:
                                                                        - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4044)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12594
    Commit: a3cf002
    Author: Network Service Mesh Bot
    Date: 2025-01-29 04:24:47 -0600
    Message:
                                                                                          - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4043)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 9aa7a15
    Author: Network Service Mesh Bot
    Date: 2025-01-29 04:23:55 -0600
    Message:
                                                                                                            - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4042)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12560
    Commit: 534b975
    Author: Network Service Mesh Bot
    Date: 2025-01-21 07:31:07 -0600
    Message:
                                                                                                                              - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4041)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/12682
    Commit: e8f3cf0
    Author: Laszlo Kiraly
    Date: 2025-01-21 10:38:08 +0100
    Message:
                                                                                                                                                - Merge pull request #4040 from Nordix/remove-unsupported-integration-tests
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- suites/k8s_monolith/external_nsc/docker/suite.gen.go
+++ suites/k8s_monolith/external_nsc/docker/suite.gen.go
@@ -27,7 +27,7 @@ func (s *Suite) SetupSuite() {
 		r.Run(`rm docker-compose.yaml`)
 	})
 	r.Run(`cat > docker-compose.override.yaml <<EOF` + "\n" + `---` + "\n" + `networks:` + "\n" + `  kind:` + "\n" + `    external: true` + "\n" + `` + "\n" + `services:` + "\n" + `  nsc-simple-docker:` + "\n" + `    networks:` + "\n" + `      - kind` + "\n" + `    environment:` + "\n" + `      NSM_NETWORK_SERVICES: kernel://kernel2ip2kernel-monolith-nsc@k8s.nsm/nsm-1` + "\n" + `EOF`)
-	r.Run(`curl https://raw.githubusercontent.com/networkservicemesh/deployments-k8s/73b805ad49e534d2f347644d214f19ede2cf4131/apps/nsc-simple-docker/docker-compose.yaml -o docker-compose.yaml`)
+	r.Run(`curl https://raw.githubusercontent.com/networkservicemesh/deployments-k8s/9449d527f7f31e3441fb90e8085b1d6d66d1f9ac/apps/nsc-simple-docker/docker-compose.yaml -o docker-compose.yaml`)
 	r.Run(`docker compose -f docker-compose.yaml -f docker-compose.override.yaml up -d`)
 }
 func (s *Suite) Test() {}
