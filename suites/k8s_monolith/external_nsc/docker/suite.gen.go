9d666123e6b21101ff501e21ad5b6dbf5c197c7b
NSMBot <nsmbot@networkservicmesh.io>
2025-02-20 12:50:27 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: f5fbe61
    Author: Network Service Mesh Bot
    Date: 2025-02-20 06:48:29 -0600
    Message:
      - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4084)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 9424fb1
    Author: Network Service Mesh Bot
    Date: 2025-02-20 06:46:19 -0600
    Message:
        - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4083)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 4724526
    Author: Network Service Mesh Bot
    Date: 2025-02-20 06:43:18 -0600
    Message:
            - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4082)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 25549f0
    Author: Network Service Mesh Bot
    Date: 2025-02-20 06:40:12 -0600
    Message:
                    - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4081)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: e825eff
    Author: Network Service Mesh Bot
    Date: 2025-02-19 08:01:23 -0600
    Message:
                                    - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4080)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 54869f8
    Author: Laszlo Kiraly
    Date: 2025-02-19 14:37:50 +0100
    Message:
                                                      - Merge pull request #4075 from networkservicemesh/dependabot/github_actions/hmarr/debug-action-3
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- suites/k8s_monolith/external_nsc/docker/suite.gen.go
+++ suites/k8s_monolith/external_nsc/docker/suite.gen.go
@@ -27,7 +27,7 @@ func (s *Suite) SetupSuite() {
 		r.Run(`rm docker-compose.yaml`)
 	})
 	r.Run(`cat > docker-compose.override.yaml <<EOF` + "\n" + `---` + "\n" + `networks:` + "\n" + `  kind:` + "\n" + `    external: true` + "\n" + `` + "\n" + `services:` + "\n" + `  nsc-simple-docker:` + "\n" + `    networks:` + "\n" + `      - kind` + "\n" + `    environment:` + "\n" + `      NSM_NETWORK_SERVICES: kernel://kernel2ip2kernel-monolith-nsc@k8s.nsm/nsm-1` + "\n" + `EOF`)
-	r.Run(`curl https://raw.githubusercontent.com/networkservicemesh/deployments-k8s/ffed61296ecfe9b5c208fab1806f74afb72c9ccf/apps/nsc-simple-docker/docker-compose.yaml -o docker-compose.yaml`)
+	r.Run(`curl https://raw.githubusercontent.com/networkservicemesh/deployments-k8s/19d35e49ea11e325d2771b47be2ce1b23be2ba68/apps/nsc-simple-docker/docker-compose.yaml -o docker-compose.yaml`)
 	r.Run(`docker compose -f docker-compose.yaml -f docker-compose.override.yaml up -d`)
 }
 func (s *Suite) Test() {}
--- suites/spire/cluster2/suite.gen.go
+++ suites/spire/cluster2/suite.gen.go
@@ -26,10 +26,10 @@ func (s *Suite) SetupSuite() {
 		r.Run(`kubectl --kubeconfig=$KUBECONFIG2 delete crd clusterspiffeids.spire.spiffe.io` + "\n" + `kubectl --kubeconfig=$KUBECONFIG2 delete crd clusterfederatedtrustdomains.spire.spiffe.io` + "\n" + `kubectl --kubeconfig=$KUBECONFIG2 delete validatingwebhookconfiguration.admissionregistration.k8s.io/spire-controller-manager-webhook` + "\n" + `kubectl --kubeconfig=$KUBECONFIG2 delete ns spire`)
 	})
 	r.Run(`[[ ! -z $KUBECONFIG2 ]]`)
-	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 apply -k https://github.com/networkservicemesh/deployments-k8s/examples/spire/cluster2?ref=ffed61296ecfe9b5c208fab1806f74afb72c9ccf`)
+	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 apply -k https://github.com/networkservicemesh/deployments-k8s/examples/spire/cluster2?ref=19d35e49ea11e325d2771b47be2ce1b23be2ba68`)
 	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 wait -n spire --timeout=3m --for=condition=ready pod -l app=spire-server`)
 	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 wait -n spire --timeout=1m --for=condition=ready pod -l app=spire-agent`)
-	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 apply -f https://raw.githubusercontent.com/networkservicemesh/deployments-k8s/ffed61296ecfe9b5c208fab1806f74afb72c9ccf/examples/spire/cluster2/clusterspiffeid-template.yaml`)
-	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 apply -f https://raw.githubusercontent.com/networkservicemesh/deployments-k8s/ffed61296ecfe9b5c208fab1806f74afb72c9ccf/examples/spire/base/clusterspiffeid-webhook-template.yaml`)
+	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 apply -f https://raw.githubusercontent.com/networkservicemesh/deployments-k8s/19d35e49ea11e325d2771b47be2ce1b23be2ba68/examples/spire/cluster2/clusterspiffeid-template.yaml`)
+	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 apply -f https://raw.githubusercontent.com/networkservicemesh/deployments-k8s/19d35e49ea11e325d2771b47be2ce1b23be2ba68/examples/spire/base/clusterspiffeid-webhook-template.yaml`)
 }
 func (s *Suite) Test() {}
