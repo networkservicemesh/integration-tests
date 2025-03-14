c113a6d460551360c6078c92ab0358a8c9ecbca2
NSMBot <nsmbot@networkservicmesh.io>
2025-03-14 13:58:58 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: f62cff9
    Author: Network Service Mesh Bot
    Date: 2025-03-14 08:57:12 -0500
    Message:
      - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4163)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: ff9dd95
    Author: Network Service Mesh Bot
    Date: 2025-03-14 08:40:08 -0500
    Message:
        - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4162)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 2fe2b0e
    Author: Network Service Mesh Bot
    Date: 2025-03-14 08:32:16 -0500
    Message:
            - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4161)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 1da054a
    Author: Network Service Mesh Bot
    Date: 2025-03-14 08:24:54 -0500
    Message:
                    - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4160)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: a4e34d4
    Author: Network Service Mesh Bot
    Date: 2025-03-14 07:35:21 -0500
    Message:
                                    - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4159)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 6f19375
    Author: Network Service Mesh Bot
    Date: 2025-03-14 07:30:40 -0500
    Message:
                                                      - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4158)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 0138da1
    Author: Network Service Mesh Bot
    Date: 2025-03-14 07:28:19 -0500
    Message:
                                                                        - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4157)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 942db4c
    Author: Network Service Mesh Bot
    Date: 2025-03-14 07:25:51 -0500
    Message:
                                                                                          - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4156)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 39de5b8
    Author: Laszlo Kiraly
    Date: 2025-03-14 13:22:04 +0100
    Message:
                                                                                                            - Merge pull request #4134 from networkservicemesh/remove-gke-from-workflows
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- suites/pss/suite.gen.go
+++ suites/pss/suite.gen.go
@@ -28,7 +28,7 @@ func (s *Suite) SetupSuite() {
 		r.Run(`kubectl delete ds/forwarder-vpp -n nsm-system`)
 		r.Run(`kubectl delete mutatingwebhookconfiguration nsm-mutating-webhook` + "\n" + `kubectl delete ns nsm-system`)
 	})
-	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/pss/nsm-system?ref=3ceeb9ac51891ef543d5267f4c665c209abf7d19`)
+	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/pss/nsm-system?ref=2964084417075b9bb7aedb869db320fd9e81c6df`)
 	r.Run(`WH=$(kubectl get pods -l app=admission-webhook-k8s -n nsm-system --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')` + "\n" + `kubectl wait --for=condition=ready --timeout=1m pod ${WH} -n nsm-system`)
 }
 func (s *Suite) TestNginx() {
@@ -36,7 +36,7 @@ func (s *Suite) TestNginx() {
 	s.T().Cleanup(func() {
 		r.Run(`kubectl delete ns ns-nginx`)
 	})
-	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/pss/use-cases/nginx?ref=3ceeb9ac51891ef543d5267f4c665c209abf7d19`)
+	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/pss/use-cases/nginx?ref=2964084417075b9bb7aedb869db320fd9e81c6df`)
 	r.Run(`kubectl wait --for=condition=ready --timeout=5m pod -l app=nse-kernel -n ns-nginx`)
 	r.Run(`kubectl wait --for=condition=ready --timeout=1m pod -l app=nettools -n ns-nginx`)
 	r.Run(`kubectl exec pods/nettools -n ns-nginx -- curl 172.16.1.100:8080 | grep -o "<title>Welcome to nginx"`)
