56ffb7500f7b330e5b6b65b05b75e49b1464dde2
NSMBot <nsmbot@networkservicmesh.io>
2025-07-17 12:38:02 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 010f3e6
    Author: Network Service Mesh Bot
    Date: 2025-07-16 10:12:54 -0500
    Message:
      - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4433)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 78a57e4
    Author: Network Service Mesh Bot
    Date: 2025-07-16 09:56:12 -0500
    Message:
        - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4432)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: e96d6d2
    Author: Network Service Mesh Bot
    Date: 2025-07-16 09:41:40 -0500
    Message:
            - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4431)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 6875819
    Author: Network Service Mesh Bot
    Date: 2025-07-16 09:39:45 -0500
    Message:
                    - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4430)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 447bdc4
    Author: Network Service Mesh Bot
    Date: 2025-07-16 09:38:36 -0500
    Message:
                                    - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4429)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: bd52cc0
    Author: Network Service Mesh Bot
    Date: 2025-07-16 09:38:28 -0500
    Message:
                                                      - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4428)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 059b344
    Author: Network Service Mesh Bot
    Date: 2025-07-16 09:37:55 -0500
    Message:
                                                                        - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4427)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 4ccae1c
    Author: Network Service Mesh Bot
    Date: 2025-07-16 09:32:58 -0500
    Message:
                                                                                          - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4426)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: e219230
    Author: Network Service Mesh Bot
    Date: 2025-07-16 09:27:45 -0500
    Message:
                                                                                                            - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4425)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 5c857d3
    Author: Network Service Mesh Bot
    Date: 2025-07-16 09:25:47 -0500
    Message:
                                                                                                                              - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4424)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 3e9754a
    Author: Network Service Mesh Bot
    Date: 2025-07-16 07:36:38 -0500
    Message:
                                                                                                                                                - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4423)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 324d15b
    Author: Laszlo Kiraly
    Date: 2025-07-15 16:08:43 +0200
    Message:
                                                                                                                                                                  - Merge pull request #4053 from Nordix/fix-man-gen-doc
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- suites/observability/nsm_system/suite.gen.go
+++ suites/observability/nsm_system/suite.gen.go
@@ -27,7 +27,7 @@ func (s *Suite) SetupSuite() {
 	s.T().Cleanup(func() {
 		r.Run(`kubectl delete mutatingwebhookconfiguration nsm-mutating-webhook` + "\n" + `kubectl delete ns nsm-system`)
 	})
-	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/observability/nsm_system?ref=ea8adfba14c70a533b05a10130f86f568aeb7f85`)
+	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/observability/nsm_system?ref=7edaebb0ec541578abc8c41e59101fb84b776f4c`)
 	r.Run(`WH=$(kubectl get pods -l app=admission-webhook-k8s -n nsm-system --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')` + "\n" + `kubectl wait --for=condition=ready --timeout=1m pod ${WH} -n nsm-system`)
 }
 func (s *Suite) Test() {}
