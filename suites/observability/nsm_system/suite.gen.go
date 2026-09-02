9e43569dc42f2e70238eb9523dc7e017463e57cc
NSMBot <nsmbot@networkservicmesh.io>
2026-09-02 12:32:51 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 9713fd8
    Author: Laszlo Kiraly
    Date: 2026-09-02 11:00:50 +0200
    Message:
      - Merge pull request #4528 from networkservicemesh/migrate/golangci-v2
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- suites/observability/nsm_system/suite.gen.go
+++ suites/observability/nsm_system/suite.gen.go
@@ -27,7 +27,7 @@ func (s *Suite) SetupSuite() {
 	s.T().Cleanup(func() {
 		r.Run(`kubectl delete mutatingwebhookconfiguration nsm-mutating-webhook` + "\n" + `kubectl delete ns nsm-system`)
 	})
-	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/observability/nsm_system?ref=270f81417e7cf0f76a4a3448373c6e7549520655`)
+	r.Run(`kubectl apply -k https://github.com/networkservicemesh/deployments-k8s/examples/observability/nsm_system?ref=116ff9c873a62a64ba5c3ea8b355b4b8475fd8f0`)
 	r.Run(`WH=$(kubectl get pods -l app=admission-webhook-k8s -n nsm-system --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')` + "\n" + `kubectl wait --for=condition=ready --timeout=1m pod ${WH} -n nsm-system`)
 }
 func (s *Suite) Test() {}
