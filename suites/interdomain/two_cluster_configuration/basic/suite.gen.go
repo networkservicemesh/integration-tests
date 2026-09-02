249ea62625cbdaf32c9df75d17bb49690af20524
NSMBot <nsmbot@networkservicmesh.io>
2026-09-02 12:38:16 +0000

    Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 8a656ef
    Author: Network Service Mesh Bot
    Date: 2026-09-02 07:37:44 -0500
    Message:
      - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4530)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: eda0185
    Author: Network Service Mesh Bot
    Date: 2026-09-02 07:35:32 -0500
    Message:
        - Update go.mod and go.sum to latest version from networkservicemesh/deployments-k8s@main (#4529)
    PR link: https://github.com/networkservicemesh/deployments-k8s/pull/
    Commit: 9713fd8
    Author: Laszlo Kiraly
    Date: 2026-09-02 11:00:50 +0200
    Message:
            - Merge pull request #4528 from networkservicemesh/migrate/golangci-v2
    Signed-off-by: NSMBot <nsmbot@networkservicmesh.io>

--- suites/interdomain/two_cluster_configuration/basic/suite.gen.go
+++ suites/interdomain/two_cluster_configuration/basic/suite.gen.go
@@ -36,8 +36,8 @@ func (s *Suite) SetupSuite() {
 		r.Run(`kubectl --kubeconfig=$KUBECONFIG1 delete mutatingwebhookconfiguration nsm-mutating-webhook` + "\n" + `kubectl --kubeconfig=$KUBECONFIG1 delete ns nsm-system`)
 		r.Run(`kubectl --kubeconfig=$KUBECONFIG2 delete mutatingwebhookconfiguration nsm-mutating-webhook` + "\n" + `kubectl --kubeconfig=$KUBECONFIG2 delete ns nsm-system`)
 	})
-	r.Run(`kubectl --kubeconfig=$KUBECONFIG1 apply -k https://github.com/networkservicemesh/deployments-k8s/examples/interdomain/two_cluster_configuration/basic/cluster1?ref=5bb16bdfddeec3b7db3191238e89c84f072d93ee`)
-	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 apply -k https://github.com/networkservicemesh/deployments-k8s/examples/interdomain/two_cluster_configuration/basic/cluster2?ref=5bb16bdfddeec3b7db3191238e89c84f072d93ee`)
+	r.Run(`kubectl --kubeconfig=$KUBECONFIG1 apply -k https://github.com/networkservicemesh/deployments-k8s/examples/interdomain/two_cluster_configuration/basic/cluster1?ref=fc3dc56f6513785d40a9717fb3bffb45f96cefc5`)
+	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 apply -k https://github.com/networkservicemesh/deployments-k8s/examples/interdomain/two_cluster_configuration/basic/cluster2?ref=fc3dc56f6513785d40a9717fb3bffb45f96cefc5`)
 	r.Run(`kubectl --kubeconfig=$KUBECONFIG1 wait --for=condition=ready --timeout=1m pod -n nsm-system -l app=admission-webhook-k8s`)
 	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 wait --for=condition=ready --timeout=1m pod -n nsm-system -l app=admission-webhook-k8s`)
 }
